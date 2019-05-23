96f95cbd59b235ae8598b67ae8e1ced25563728a
fred21 <1394274890@qq.com>
2019-05-23 23:51:32 +0800

    fred21 update

--- sig_re.py
+++ sig_re.py
@@ -2,6 +2,7 @@ import os
 import signal    
 from time import sleep    
 # comment add by hwfred21     
+#comment : add by fred21 2019-5-23
 def onsignal_term(a,b):    
 	print 'get SIGTERM'    
      
